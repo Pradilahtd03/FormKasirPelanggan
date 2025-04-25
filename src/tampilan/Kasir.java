94187f7e12d66eba791b4a2be7eca76644e56cf6
Pradilahtd03 <pradilahtahtadhilika15@gmail.com>
2025-04-26 00:14:04 +0700

    Pesan commit kamu

--- src/tampilan/Kasir.java
+++ src/tampilan/Kasir.java
@@ -11,7 +11,7 @@ import koneksi.koneksi;
 
 /**
  *
- * @author sheila
+ * @author Pradilah Tahta Dhilika
  */
 public class Kasir extends javax.swing.JFrame {
     private Connection conn = new koneksi().connect();
